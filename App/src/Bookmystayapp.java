e901140bd46ab53a2863a4ef1acadd49033ffe9e
JatinWahane <jatinkumarwahane@gmail.com>
2026-03-30 08:46:01 +0530

    UC1 Done

---
+++ App/src/Bookmystayapp.java
@@ -0,0 +1,11 @@
+public class Bookmystayapp {
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("   Welcome to Book My Stay App");
+        System.out.println("   Hotel Booking System v1.0");
+        System.out.println("=====================================");
+
+        System.out.println("Application started successfully and terminated.");
+    }
+}
